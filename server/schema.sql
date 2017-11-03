0ccf56edbc1dfc8ed484dae99daee9cd5cce722d
Hack Reactor Student <student@hackreactor.com>
2017-11-03 16:57:27 -0700

    Created schema, got the client setup, and other init stuff

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  name CHAR(25)
+);
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  name CHAR(25)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  message CHAR(140), 
+  username_id INT,
+  room_id INT,
+  FOREIGN KEY (username_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
